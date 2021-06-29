cf5d5f04532b4d41a5e472e3af4197f1f7095ed5
mithem <mithem@github.com>
2021-06-29 09:44:44 +0200

    Add pyscript (hassio:HACS) to create task in Things to update hass when appropriate

---
+++ create_task_in_things_to_update_hass.py
@@ -0,0 +1,15 @@
+# a script meant to be invoked by pyscript (available via HACS)
+
+import aiohttp
+
+url = "http://192.168.2.197:8001/api/v1/create-things-task-to-update-hass"
+
+@service
+def create_task_in_things_to_update_hass():
+    log.debug("Trying to create task in things to update hass.")
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url) as r:
+            if r.ok:
+                log.info("Successfully created task in Things.")
+            else:
+                log.error(f"Invalid response: {r.text}")
