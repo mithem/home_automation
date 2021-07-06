96234946ee7a1fc05ea59bb9c75c52c5548402d7
mithem <mithem@github.com>
2021-07-06 20:57:27 +0200

    Refactor config loading & add tests for just that

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # just need to implement this by myself as dotenv isn't easily available on
 # Synology DSM.
 import os
+from typing import List, Dict
 
 _required_keys = [
     "EMAIL_ADDRESS",
@@ -16,23 +17,37 @@ class ConfigError(Exception):
     pass
 
 
-def load_dotenv():
-    def load(s: str):
-        with open(s, "r") as f:
-            lines = f.readlines()
-        for line in lines:
-            varname, value = line.split("=")
-            os.environ[varname] = value.replace("\n", "")
-    try:
-        load(".env")
-    except FileNotFoundError:
-        load("/volume2/repos/home-automation/.env")
+def parse_config(lines: List[str]) -> Dict[str, str]:
+    d = {}
+    for line in lines:
+        key, value = line.split("=")
+        if value.endswith("\n"):
+            value = value.replace("\n", "")
+        d[key] = value
+
     keys_not_found = []
+    keys = d.keys()
     for key in _required_keys:
-        value = os.environ.get(key)
-        if value is None:
+        if key not in keys:
             keys_not_found.append(key)
+
     if len(keys_not_found):
         raise ConfigError(
             "The following variables could not be found in the environment: "
             + ", ".join(keys_not_found))
+    return d
+
+
+def load_into_environment(config: Dict[str, str]):
+    for key, value in config.items():
+        os.environ[key] = value
+
+
+def load_dotenv():
+    def load(path: str):
+        with open(path, "r") as f:
+            load_into_environment(parse_config(f.readlines()))
+    try:
+        load(".env")
+    except FileNotFoundError:
+        load("/volume2/repos/home-automation/.env")
---
+++ test_config.py
@@ -0,0 +1,47 @@
+from config import parse_config, load_into_environment, ConfigError
+import pytest
+import os
+
+valid_config = [
+    "EMAIL_ADDRESS=hello@github.com",
+    "EMAIL_PASSWD=passw0rd1",
+    "LOG_DIR=/var/logs\n",
+    "HASS_TOKEN=abcABC123\n",
+    "HASS_BASE_URL=http://homeassistant.local:8123",
+    "THINGS_SERVER_URL=http://192.168.2.197:8001\n",
+    "ANOTHER=hello"
+]
+
+
+def test_parse_config_valid_config():
+    expected = {
+        "EMAIL_ADDRESS": "hello@github.com",
+        "EMAIL_PASSWD": "passw0rd1",
+        "LOG_DIR": "/var/logs",
+        "HASS_TOKEN": "abcABC123",
+        "HASS_BASE_URL": "http://homeassistant.local:8123",
+        "THINGS_SERVER_URL": "http://192.168.2.197:8001",
+        "ANOTHER": "hello"
+    }
+
+    result = parse_config(valid_config)
+
+    assert result == expected
+
+
+def test_parse_config_raises_exception_when_config_incomplete():
+    config = [
+        "EMAIL_ADDRESS=hello@github.com",
+        "EMAIL_PASSWD=passw0rd1"
+    ]
+    with pytest.raises(ConfigError):
+        parse_config(config)
+
+
+def test_load_into_environment():
+    # for that, it's fine to use a function tested somewhere else
+    config = parse_config(valid_config)
+    load_into_environment(config)
+
+    for key, value in config.items():
+        assert os.environ[key] == value
