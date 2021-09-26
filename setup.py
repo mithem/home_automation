35d7030a41c31b9650413fda609da27ca3f5fdb4
mithem <mithem@github.com>
2021-09-26 06:59:41 +0200

    Packaging improvements
    - automatically update the VERSION var on home_automation(.__init__)
    - readd pylint as a dependency
    - use async extra for flask

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 import os
 from setuptools import setup
+import re
 
 VERSION = "1.0.0"
+
+with open("home_automation/__init__.py", "r") as f:
+    code = "\n".join(f.readlines())
+
+code = re.sub(r"""VERSION ?= ?("|')\d+\.\d+\.\d+("|')""", "VERSION = \"" + VERSION + "\"", code)
+
+with open("home_automation/__init__.py", "w") as f:
+    f.flush()
+    f.writelines(code.split("\n"))
 
 packages = ["home_automation", "home_automation.server.backend"]
 
 requirements = [
     "argparse",
-    "flask",
+    "flask[async]",
     "yagmail",
     "fileloghelper",
     "httpx>=0.18,<0.20",
@@ -17,6 +27,7 @@ requirements = [
     "pytest-httpx",
     "pytest-flask",
     "pyfakefs",
+    "pylint",
     "pylint-exit",
     "aiohttp",
     "mypy",
