12c7cd7d1e88ff32b7006696a402a6cd7ad26dc9
mithem <mithem@github.com>
2021-11-28 09:56:08 +0100

    1.1.0-b16.
    - alternative docker-compose for pulling from the GitLab registry
    - automatic version tagging for docker images

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import re
 import home_automation
 
-VERSION = "1.1.0-b15"
+VERSION = "1.1.0-b16"
 
 with open("home_automation/__init__.py", "r") as f:
     code = f.read()
@@ -12,6 +12,9 @@ code = code.replace(home_automation.VERSION, VERSION, 1)
 
 with open("home_automation/__init__.py", "w") as f:
     f.write(code)
+
+with open("VERSION", "w") as f:
+    f.write(VERSION)
 
 packages = ["home_automation", "home_automation.server.backend"]
 
