6e37ccc838f5beb3dfbddb2eec50a7c200d694ad
mithem <mithem@github.com>
2022-03-09 13:31:47 +0100

    1.1.2-a1
    in-between stuff

--- home_automation/config.py
+++ home_automation/config.py
@@ -1,120 +1,235 @@
 """A workaround for dotenv not being available on Synology DSM."""
 import os
 import re
-from typing import List, Dict
+from turtle import st
+import yaml
+from typing import Any, List, Dict, Optional
 
-from fileloghelper import Logger
+from logging import Logger
 
-_required_keys = [
-    "EMAIL_ADDRESS",
-    "EMAIL_PASSWD",
-    "HASS_TOKEN",
-    "HASS_BASE_URL",
-    "THINGS_SERVER_URL",
-    "LOG_DIR",
-    "HOMEWORK_DIR",
-    "ARCHIVE_DIR",
-    "DB_PATH",
-    "COMPOSE_FILE",
-    "MOODLE_DL_DIR",
-]
+
+class ConfigEmail:
+    """Email configuration."""
+    address: str
+    password: str
+
+    def __new__(self, address: str, password: str):
+        self.address = address
+        self.password = password
+        return self
+
+    def __str__(self) -> str:
+        return str(vars(self))
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.address == other.address and
+            self.password == other.password
+        )
+
+
+class ConfigHomeAssistant:
+    """Home Assistant configuration."""
+    token: str
+    base_url: str
+    insecure_https: bool
+
+    def __new__(self, data: Dict[str, str]):
+        token = data.get("token")
+        base_url = data.get("base_url")
+        insecure_https = data.get("insecure_https", False)
+
+        if not token or not base_url:
+            return None
+
+        self.token = token
+        self.base_url = base_url
+        self.insecure_https = insecure_https
+        return self
+
+    def __str__(self) -> str:
+        return str(vars(self))
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.token == other.token and
+            self.base_url == other.base_url and
+            self.insecure_https == other.insecure_https
+        )
+
+
+class ConfigPortainer:
+    """Portainer configuration."""
+    url: str
+    username: str
+    password: str
+    insecure_https: bool
+
+    def __new__(self, data: Dict[str, str]):
+        url = data.get("url")
+        username = data.get("username")
+        password = data.get("password")
+        insecure_https = data.get("insecure_https", False)
+
+        if not url or not username or not password:
+            return None
+
+        self.url = url
+        self.username = username
+        self.password = password
+        self.insecure_https = insecure_https
+        return self
+
+    def __str__(self) -> str:
+        return str(vars(self))
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.url == other.url and
+            self.username == other.username and
+            self.password == other.password and
+            self.insecure_https == other.insecure_https
+        )
+
+
+class ConfigThingsServer:
+    """Things server configuration."""
+    url: str
+    insecure_https: bool
+
+    def __new__(self, data: Dict[str, str]):
+        url = data.get("url")
+        insecure_https = data.get("insecure_https", False)
+
+        if not url:
+            return None
+
+        self.url = url
+        self.insecure_https = insecure_https
+        return self
+
+    def __str__(self) -> str:
+        return str(vars(self))
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.url == other.url and
+            self.insecure_https == other.insecure_https
+        )
+
+
+class ConfigProcess:
+    """Configuration of the home_automation process."""
+    user: Optional[str]
+    group: Optional[str]
+
+    def __new__(self, data: Dict[str, str]):
+        self.user = data.get("user")
+        self.group = data.get("group")
+
+        if not self.user and not self.group:
+            return None
+
+        return self
+
+    def __str__(self) -> str:
+        return str(vars(self))
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.user == other.user and
+            self.group == other.group
+        )
+
+
+class Config:
+    """Configuration data."""
+    log_dir: str
+    homework_dir: str
+    archive_dir: str
+    db_path: str
+    compose_file: str
+    extra_compress_dirs: List[str]
+    moodle_dl_dir: Optional[str]
+    email: ConfigEmail
+    home_assistant: Optional[ConfigHomeAssistant]
+    portainer: Optional[ConfigPortainer]
+    things_server: Optional[ConfigThingsServer]
+    process: Optional[ConfigProcess]
+
+    def __init__(self, log_dir: str,
+                 homework_dir: str,
+                 archive_dir: str,
+                 db_path: str,
+                 compose_file: str,
+                 email: Dict[str, str],
+                 home_assistant: Dict[str, Any] = dict(),
+                 portainer: Dict[str, Any] = dict(),
+                 things_server: Dict[str, Any] = dict(),
+                 process: Dict[str, str] = dict(),
+                 extra_compress_dirs: List[str] = [],
+                 moodle_dl_dir: Optional[str] = None
+                 ):
+        self.log_dir = log_dir
+        self.homework_dir = homework_dir
+        self.archive_dir = archive_dir
+        self.db_path = db_path
+        self.compose_file = compose_file
+        self.moodle_dl_dir = moodle_dl_dir
+        self.extra_compress_dirs = extra_compress_dirs
+        self.email = ConfigEmail(**email)
+        self.home_assistant = ConfigHomeAssistant(home_assistant)
+        self.portainer = ConfigPortainer(portainer)
+        self.things_server = ConfigThingsServer(things_server)
+        self.process = ConfigProcess(process)
+
+    def __str__(self) -> str:
+        return str(vars(self))
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.log_dir == other.log_dir and
+            self.homework_dir == other.homework_dir and
+            self.archive_dir == other.archive_dir and
+            self.db_path == other.db_path and
+            self.compose_file == other.compose_file and
+            self.moodle_dl_dir == other.moodle_dl_dir and
+            self.email == other.email and
+            self.home_assistant == other.home_assistant and
+            self.portainer == other.portainer and
+            self.things_server == other.things_server and
+            self.process == other.process
+        )
 
 
 class ConfigError(Exception):
     """An exception thrown when the config is oncomplete or invalid."""
 
 
-def get_config_error(keys_not_found):
-    """Prepare and return a ConfigError with the appropriate error message."""
-    return ConfigError(
-        "The following variables could not be found in the environment: "
-        + ", ".join(keys_not_found))
+def load_config(logger: Logger = None, path: str = "home_automation.conf.yml") -> Config:
+    """Load config from file and return it."""
+    with open(path, "r", encoding="utf-8") as file_obj:
+        return parse_config(file_obj.read())
 
 
-def parse_config(lines: List[str]) -> Dict[str, str]:
-    """Parse config and return parsed dict."""
-    env_keys = os.environ.keys()
-    config = {}
-    for line in lines:
-        match = re.match(r"(?P<key>[\w_]+)\s?=\s?(?P<value>.*)\n?", line)
-        if match:
-            groupdict = match.groupdict()
-            key = groupdict.get("key")
-            value = groupdict.get("value")
-            if value:
-                for char in [" ", "\"", "'"]:
-                    value = value.strip(char)
-            if key:
-                if value and value:
-                    config[str(key)] = str(value)
-
-    keys_not_found = []
-    keys = config.keys()
-    for key in _required_keys:
-        if key not in keys and key not in env_keys:
-            keys_not_found.append(key)
-
-    if keys_not_found:
-        raise get_config_error(keys_not_found)
-    return config
-
-
-def load_into_environment(
-        config: Dict[str, str],
-        config_to_parse_from_file: List[str] = None,
-        force=False
-):
-    """Load the dict into the environment."""
-    if config_to_parse_from_file is not None:
-        for key, value in config.items():
-            if key in config_to_parse_from_file:
-                if force:
-                    os.environ[key] = value
-                else:
-                    existing_value = os.environ.get(key, None)
-                    if not existing_value:
-                        os.environ[key] = value
-    else:
-        for key, value in config.items():
-            os.environ[key] = value
-
-
-def load_dotenv(logger: Logger = None):
-    """Check if environment contains required values, otherwise
-    find the appropriate file and load it's content into the environment."""
-
-    def check() -> List[str]:
-        config_to_parse_from_file = []
-        for key in _required_keys:
-            value = os.environ.get(key, None)
-            if not value:
-                config_to_parse_from_file.append(key)
-        return config_to_parse_from_file
-
-    config_to_parse_from_file = check()
-    if not config_to_parse_from_file:
-        return  # no need to search for .env
-
-    try_to_load = [
-        ".env",
-        "../.env",
-        "/mnt/FastStorage/mithem-applications/home_automation/.env",
-        "/home_automation/env"
-    ]
-    loaded = False
-    for path in try_to_load:
-        try:
-            with open(path, "r", encoding="utf-8") as file_obj:
-                load_into_environment(parse_config(
-                    file_obj.readlines()), config_to_parse_from_file)
-                if logger:
-                    logger.context = "config"
-                    logger.info(f"Loaded '{path}'.")
-            loaded = True
-            break
-        except FileNotFoundError:
-            continue
-
-    if not loaded:
-        raise ConfigError("Could not find .env file.") from get_config_error(
-            [])  # no file -> no lines -> envvars insufficient
+def parse_config(config: str) -> Config:
+    config = yaml.safe_load(config)
+    return Config(**config)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,175 +1,36 @@
+import email
 import os
 
 import pytest
-import pytest_asyncio
-from home_automation.config import (ConfigError, load_into_environment,
-                                    parse_config, load_dotenv, _required_keys)
-
-VALID_CONFIG_LINES = [
-    "EMAIL_ADDRESS=hello@github.com",
-    "EMAIL_PASSWD=passw0rd1",
-    "LOG_DIR=/var/logs\n",
-    "HASS_TOKEN=abcABC123\n",
-    "HASS_BASE_URL=\"http://homeassistant.local:8123\"",
-    "THINGS_SERVER_URL=http://192.168.2.197:8001\n",
-    "HOMEWORK_DIR = /volume2/Hausaufgaben/HAs",
-    "ARCHIVE_DIR = '/volume2/Hausaufgaben/Archive'",
-    "ANOTHER=\" hello\"",
-    "INSECURE_HTTPS=0",
-    "DB_PATH= home_automation.backend.db",
-    "COMPOSE_FILE= docker-compose.yml ",
-    "MOODLE_DL_DIR=~/moodle",
-    "EXTRA_COMPRESS_DIRS=/mnt/extra1;/mnt/extra2",
-    "HOME_AUTOMATION_USER=",
-    "HOME_AUTOMATION_GROUP=wheel"
-]
-
-VALID_CONFIG_DICT = {
-    "EMAIL_ADDRESS": "hello@github.com",
-    "EMAIL_PASSWD": "passw0rd1",
-    "LOG_DIR": "/var/logs",
-    "HASS_TOKEN": "abcABC123",
-    "HASS_BASE_URL": "http://homeassistant.local:8123",
-    "THINGS_SERVER_URL": "http://192.168.2.197:8001",
-    "HOMEWORK_DIR": "/volume2/Hausaufgaben/HAs",
-    "ARCHIVE_DIR": "/volume2/Hausaufgaben/Archive",
-    "ANOTHER": " hello",
-    "INSECURE_HTTPS": "0",
-    "DB_PATH": "home_automation.backend.db",
-    "COMPOSE_FILE": "docker-compose.yml",
-    "MOODLE_DL_DIR": "~/moodle",
-    "EXTRA_COMPRESS_DIRS": "/mnt/extra1;/mnt/extra2",
-    "HOME_AUTOMATION_GROUP": "wheel"
-}
+from home_automation.config import ConfigEmail, parse_config, Config
 
 
-@pytest.mark.usefixtures("fixture_delete_environment_vars")
-class EnvironmentSensibleTestCase():
-    @pytest_asyncio.fixture
-    def fixture_delete_environment_vars(self):
-        self.delete_environment_vars()
+class TestConfig:
+    def test_parse_config(self):
+        config = """
+        log_dir: /var/log/home_automation
+        homework_dir: /mnt/MassStorage/Hausaufgaben
+        archive_dir: /mnt/MassStorage/Hausaufgaben/Archive
+        db_path: ./home_automation.backend.db
+        compose_file: /mnt/FastStorage/docker-compose.yml
+        email:
+            address: 'hello@example.com'
+            password: "helloworld!"
+        """
 
-    def delete_environment_vars(self):
-        for key in _required_keys:
-            try:
-                del os.environ[key]
-            except KeyError:
-                continue
+        expected = Config(
+            log_dir="/var/log/home_automation",
+            homework_dir="/mnt/MassStorage/Hausaufgaben",
+            archive_dir="/mnt/MassStorage/Hausaufgaben/Archive",
+            db_path="./home_automation.backend.db",
+            compose_file="/mnt/FastStorage/docker-compose.y ml",
+            email={"address": "hello@example.com", "password": "helloWorld!"}
+        )
 
+        print(expected.email)
 
-class TestEnvironmentSensibleTestCase(EnvironmentSensibleTestCase):
-    def assert_no_env_vars(self):
-        for key in _required_keys:
-            assert not os.environ.get(key, None)
+        result = parse_config(config)
 
-    def test_environment_sensible_test_case_removes_env_vars(self):
-        for key in _required_keys:
-            os.environ[key] = "Hello, world!"
-        self.delete_environment_vars()
-        self.assert_no_env_vars()
+        print(result.email)
 
-    def test_environment_sensible_test_case_uses_fixture(self):
-        self.assert_no_env_vars()
-
-
-class TestParseConfig(EnvironmentSensibleTestCase):
-    def test_parse_config_valid_config(self):
-
-        result = parse_config(VALID_CONFIG_LINES)
-
-        assert result == VALID_CONFIG_DICT
-
-    def test_parse_config_raises_exception_when_config_incomplete(self):
-        config = [
-            "EMAIL_ADDRESS=hello@github.com",
-            "EMAIL_PASSWD=passw0rd1"
-        ]
-        try:
-            # make sure envvars don't allow incomplete config
-            del os.environ["ARCHIVE_DIR"]
-        except KeyError:
-            pass
-        with pytest.raises(ConfigError) as excinfo:
-            parse_config(config)
-        assert "ARCHIVE_DIR" in str(excinfo.value)
-
-
-class TestLoadIntoEnvironment(EnvironmentSensibleTestCase):
-    def test_load_into_environment_entire_config(self):
-        # for that, it's fine to use a function tested somewhere else
-        load_into_environment(VALID_CONFIG_DICT)
-
-        for key, value in VALID_CONFIG_DICT.items():
-            assert os.environ[key] == value
-
-    def test_load_into_environment_only_specified_keys(self):
-        keys_to_load = ["LOG_DIR", "HOMEWORK_DIR"]
-
-        load_into_environment(VALID_CONFIG_DICT, keys_to_load)
-
-        for key, value in VALID_CONFIG_DICT.items():
-            if key in keys_to_load:
-                assert os.environ[key] == value
-            elif key in _required_keys:  # only those are deleted by the fixture
-                with pytest.raises(KeyError):
-                    # don't need to assert, just can't think of anything else to do with the value
-                    assert not os.environ[key]
-
-    def test_load_into_environment_doesnt_override_existing_envvars(self):
-        """Of course, only when `force == False`."""
-        load_into_environment(VALID_CONFIG_DICT)
-        email_address = "test@example.com"
-        os.environ["EMAIL_ADDRESS"] = email_address
-        expected = VALID_CONFIG_DICT
-        expected["EMAIL_ADDRESS"] = email_address
-
-        load_into_environment(VALID_CONFIG_DICT)
-
-        for key, value in expected.items():
-            assert os.environ[key] == value
-
-    def test_load_into_environment_overrides_existing_envvars(self):
-        """Of course, only when `force == True`."""
-        load_into_environment(VALID_CONFIG_DICT)
-        email_address = "test@example.com"
-        os.environ["EMAIL_ADDRESS"] = email_address
-
-        load_into_environment(VALID_CONFIG_DICT, force=True)
-
-        for key, value in VALID_CONFIG_DICT.items():
-            assert os.environ[key] == value
-
-
-class TestLoadDotenv(EnvironmentSensibleTestCase):
-    def test_load_dotenv_doesnt_override_env_values(self):
-        mail = "test@example.com"
-        os.environ["EMAIL_ADDRESS"] = mail
-        # just restore to how it was before the test was run, has nothing to do with actually asserting the right functionality
-        lines_to_restore = None
-        corrected_lines = [
-            line + ("\n" if not line.endswith("\n") else "") for line in VALID_CONFIG_LINES]
-
-        try:
-            with open(".env", "r") as f:
-                lines_to_restore = f.readlines()
-        except FileNotFoundError:
-            pass
-
-        with open(".env", "w") as f:
-            f.flush()
-            f.writelines(corrected_lines)
-
-        load_dotenv()
-
-        for key, value in VALID_CONFIG_DICT.items():
-            if key != "EMAIL_ADDRESS":
-                assert os.environ[key] == value
-            else:
-                assert os.environ["EMAIL_ADDRESS"] == mail
-
-        if lines_to_restore:
-            with open(".env", "w") as f:
-                f.flush()
-                f.writelines(lines_to_restore)
-        else:
-            os.remove(".env")
+        assert result == expected
